b515ce57db5475e3ee1e6b5428e1480dca7fe681
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2021-02-25 21:25:16 +0100

    remove typescript warning for no-explicit-any

--- src/core/core.ts
+++ src/core/core.ts
@@ -20,7 +20,9 @@ let scrollAnimationFrame: number;
 /**
  * Maximize Browser Support of requestAnimationFrame
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const reqAnimFrame = w.requestAnimationFrame || (w as any).mozRequestAnimationFrame || w.webkitRequestAnimationFrame || (w as any).msRequestAnimationFrame;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const cancelAnimFrame = w.cancelAnimationFrame || (w as any).mozCancelAnimationFrame;
 
 interface ScrollToSmoothSettings {
